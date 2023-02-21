befd36eed5671036ae11ee29b6d501c818979c34
alexeira <alex.pincheira06@gmail.com>
2023-02-20 23:58:54 -0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
 
+const TURN = {
+  X: 'X',
+  O: 'O'
+}
+
+const board = Array(9).fill(null)
+
 export default function App() {
   return (
     <main>
-      <h1>Hola</h1>
+      <h1>Tic Tac Toe</h1>
     </main>
   )
 }
